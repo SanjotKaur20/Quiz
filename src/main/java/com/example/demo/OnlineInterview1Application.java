2cf01c92860521f845b3095558c7922dbdf65bed
Sanjot Kaur <166702050+SanjotKaur20@users.noreply.github.com>
2025-07-15 13:23:18 +0530

    123

---
+++ src/main/java/com/example/demo/OnlineInterview1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineInterview1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineInterview1Application.class, args);
+	}
+
+}
